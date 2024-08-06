c7246cc14a00c620524d0aceb1fa331e616c3584
preotrix-prabhakar <prabhakar8859@gmail.com>
2024-08-06 19:26:11 +0530

    protected

--- oop.cpp
+++ oop.cpp
@@ -2,56 +2,27 @@
 #include <string>
 using namespace std;
 
-class Teacher{
-
+class A{
     private:
-    double salary;
-
+    int privateA=10;
     public:
-    Teacher(){
-        // cout<<"hi am a constructor"<<endl;
-        dept="matallurgy";
-    }
-    //attributes
-    string name;
-    string dept;
-    string subject;
-    //methods member functions
-    void changeDept(string newDept){
-        dept=newDept;
-    }
-    //setter
-    void setSalary(int s){
-        salary=s;
-    }
-    //getter
-    double getSalary(){
-        return salary;
-    }
-    void getInfo(){
-        cout<<"name is "<<this->name<<endl;
-        cout<<"dept is "<<this->dept;
-        
+    int publicA=20;
+    protected:
+    
+    int protectedA=30;
+    void show(){
+        cout<<protectedA<<endl;
     }
 };
-class Account{
-    private:
-    double balance;
-    string password;
 
+class B:public A{
     public:
-    string accountId;
-    string username;
+    void test(){
+
+    }
 };
 int main(){
-Teacher t1;
-    t1.name="sdfs";
-    t1.setSalary(50000);
-    cout<<t1.name<<endl;
-    cout<<t1.dept<<endl;
-    cout<<t1.getSalary();
-    Teacher t2(t1);
-    t2.getInfo();
-
-return 0;
+    B objB;
+    cout<<objB.show();
+    return 0;
 }
