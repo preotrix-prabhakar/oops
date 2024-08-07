81e3b336d9a96d1756390c6e672af8520db9a517
preotrix-prabhakar <prabhakar8859@gmail.com>
2024-08-07 20:34:10 +0530

    protected function

--- oop.cpp
+++ oop.cpp
@@ -4,25 +4,50 @@ using namespace std;
 
 class A{
     private:
+
     int privateA=10;
+
     public:
-    int publicA=20;
+
+    int publicA=40;
+    void show(){
+        cout<<"calling sum of class A\n";
+        cout<<"showing protected A:"<<protectedA<<endl;
+        cout<<sum();
+    }
     protected:
     
     int protectedA=30;
-    void show(){
-        cout<<protectedA<<endl;
+    int sum(){
+        cout<<"showing protected member function:";
+        return privateA+publicA;
     }
 };
 
 class B:public A{
+    private:
+    int privateB=210;
     public:
-    void test(){
+    int publicB=200;
 
+    int access(){
+        cout<<"publicA is:"<<publicA<<endl;
+        cout<<"protectedA is:"<<protectedA<<endl;
+        // cout<<test()<<endl;
+
+    }
+    protected:
+    int protectedB=220;
+    int  test(){
+         return sum();
     }
 };
 int main(){
+    A objA;
+    // obja.show();
     B objB;
-    cout<<objB.show();
+    // cout<<objB.access();
+    // cout<<objA.sum();
+ 
     return 0;
 }
